d0bc2063fea6a39e2a63536265a5157d5e6a4415
rekermann <38425112+rekermann@users.noreply.github.com>
2020-08-24 00:39:12 +0200

    Update fuGameReactor.js

--- fuGameReactor.js
+++ fuGameReactor.js
@@ -1,2 +1,14 @@
 document.cookie = "sid=lolnoty; path=/";
-document.cookie = "countdownToAd=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+document.cookie = "countdownToAd=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+try {
+  var x = document.getElementById("interstitial");
+  if(x){
+	var y = document.getElementsByClassName("buttonBox continue");
+	ref = y[0].href;
+	window.location.href = ref;
+}
+}
+catch  {
+
+}
